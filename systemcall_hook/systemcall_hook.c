9238e636f61f0f0f5f08dad237e61f773dc08707
u2i <pv3f3wii@s.okayama-u.ac.jp>
2021-01-17 08:30:55 +0900

    add system call hook module which replace sys_call_table

--- systemcall_hook/systemcall_hook.c
+++ systemcall_hook/systemcall_hook.c
@@ -2,7 +2,10 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/kprobes.h>
+#include <asm/pgtable.h>
+#include <linux/utsname.h>
 
+MODULE_DESCRIPTION("system call replace test module");
 MODULE_AUTHOR("u2i");
 MODULE_LICENSE("GPL");
 
@@ -13,8 +16,50 @@ static struct kprobe kp = {
     .symbol_name = "kallsyms_lookup_name"
 };
 
+asmlinkage long (*orig_sys_uname)(struct old_utsname __user *name);
+
+static void
+save_original_syscall_address(void)
+{
+    orig_sys_uname = syscall_table[__NR_uname];
+}
+
+static void
+change_page_attr_to_rw(pte_t *pte)
+{
+    set_pte_atomic(pte, pte_mkwrite(*pte));
+}
+
+static void
+change_page_attr_to_ro(pte_t *pte)
+{
+    set_pte_atomic(pte, pte_clear_flags(*pte, _PAGE_RW));
+}
+
+static void
+replace_system_call(void *new)
+{
+    unsigned int level = 0;
+    pte_t *pte;
+
+    pte = lookup_address((unsigned long) syscall_table, &level);
+    /* Need to set r/w to a page which syscall_tabel is in. */
+    change_page_attr_to_rw(pte);
+
+    syscall_table[__NR_uname] = new;
+    /* set back to read only */
+    change_page_attr_to_ro(pte);
+}
+
+asmlinkage long
+syscall_replace_sys_uname(struct old_utsname __user *name)
+{
+    pr_info("call original uname system call\n");
+    return orig_sys_uname(name);
+}
+
 static int
-systemcall_hook_init(void)
+syscall_replace_init(void)
 {
     register_kprobe(&kp);
     printk("kp.addr: Found at %px \n", kp.addr);
@@ -23,16 +68,24 @@ systemcall_hook_init(void)
     unregister_kprobe(&kp);
 
     syscall_table = (void *)kallsyms_lookup_name_addr("sys_call_table");
-    printk("sys_call_table: Found at %px \n", syscall_table);
+    pr_info("sys_call_table address is 0x%px \n", syscall_table);
 
+    save_original_syscall_address();
+    pr_info("original sys_uname's address is %px\n", orig_sys_uname);
+
+    replace_system_call(syscall_replace_sys_uname);
+
+    pr_info("system call replaced\n");
     return 0;
 }
 
 static void
-systemcall_hook_cleanup(void)
+syscall_replace_cleanup(void)
 {
     pr_info("cleanup");
+    if (orig_sys_uname)
+        replace_system_call(orig_sys_uname);
 }
 
-module_init(systemcall_hook_init);
-module_exit(systemcall_hook_cleanup);
+module_init(syscall_replace_init);
+module_exit(syscall_replace_cleanup);
